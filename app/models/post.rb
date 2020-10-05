dde2098e5664db6bde9f2bdb5709ff34b842dc09
Yoselyn Carolina <ycarolina692@gmail.com>
2020-10-04 21:58:08 -0400

    Segundo commit,contiene relaciones entre tablas y validaciones

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    has_many :comments ,dependent: :destroy
+    belongs_to :user
+    validates_presence_of :title
+    validates_presence_of :body
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_031357) do
+ActiveRecord::Schema.define(version: 2020_10_03_031259) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -25,7 +27,18 @@ ActiveRecord::Schema.define(version: 2020_09_17_031357) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users"
 end
